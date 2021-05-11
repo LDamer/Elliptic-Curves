809919d569fbc1cd7ba0baaa135aef31e72ee00b
LDamer <leon@damers.de>
2021-05-11 18:50:42 +0200

    bugfixes i think

--- ellipticCurveGroup.py
+++ ellipticCurveGroup.py
@@ -36,27 +36,34 @@ class ECurve:
     def ord_element(self, a):
         result = a
         counter = 1
-        try:
-            while not result == (self.n, self.n):
-                result = self.add(result, a)
-                counter += 1
-            return counter
-        except:
-            print("ord(", a, ") = ", "infinity")
+        while not result == (self.n, self.n):
+            result = self.add(result, a)
+            counter += 1
+        return counter
 
     def add(self, P, Q):
+        # Adjust the values of P and Q to the underliying finite field
+        # in case one of their coordinates is negative
+        P[0] = P[0] % self.p
+        P[0] = P[1] % self.p
+        Q[0] = Q[0] % self.p
+        Q[0] = Q[1] % self.p
+
         # Check for correct inputs. Both inputs must be of type tupel.
         if type(P) != type(Q) or type(P) != tuple:
             raise Exception("x and y must be tuples!")
 
-        # Check whether the points lie on the curve.
-        # The points must fulfill the formula or they are the neutral element.
-        if ((P[0] ** 3 + self.a * P[0] + self.b) % self.p != (P[1] ** 2) % self.p
-            or (Q[0] ** 3 + self.a * Q[0] + self.b) % self.p != (Q[1] ** 2) % self.p) \
-                and P != (self.n, self.n) and Q != (self.n, self.n):
-            raise Exception("P or Q is not part of the curve!")
+        # P must be part of the curve or has to be the neutral element.
+        if P != (self.n, self.n):
+            if (P[0] ** 3 + self.a * P[0] + self.b) % self.p != (P[1] ** 2) % self.p:
+                raise Exception("P is not part of the curve!")
+
+        # Q must be part of the curve or has to be the neutral element.
+        if Q != (self.n, self.n):
+            if (Q[0] ** 3 + self.a * Q[0] + self.b) % self.p != (Q[1] ** 2) % self.p:
+                raise Exception("Q is not part of the curve!")
+
         # Check whether P is the neutral element.
-        # Therefore, comparing just the x coordiante is unambigously. 
         if P == (self.n, self.n):
             return Q
 
@@ -68,7 +75,7 @@ class ECurve:
         elif P[0] == Q[0] and -P[1] % self.p == Q[1]:
             return self.n, self.n
         else:
-            # Now is guarenteed that neither of the points is the neutral Element. 
+            # Now is guarenteed that neither of the points is the neutral Element and they both lie on the curve.
             if P == Q:
                 # In this case we need to perform point multiplication.
                 s = ((3 * P[0] ** 2 + self.a) * pow(2 * P[1], -1, self.p)) % self.p
@@ -81,11 +88,3 @@ class ECurve:
                 x_3 = (s ** 2 - P[0] - Q[0]) % self.p
                 y_3 = (s * (P[0] - x_3) - P[1]) % self.p
                 return x_3, y_3
-
-
-ec = ECurve(4, 7, 17, -1)
-
-p = (16, 11)
-
-print(ec.ord_element(p))
-print(ec.ord_group())
